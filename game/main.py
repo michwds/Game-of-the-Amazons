2e58ec5a1f6b0c40242ee6c3e56d1e9e24a1d352
Michwds <michael.woods@live.ca>
2023-09-16 14:45:19 -0700

    updated asset list added main board draw

--- game/main.py
+++ game/main.py
@@ -1,4 +1,5 @@
 from game import engine
+from engine import Piece
 import pygame as pg
 
 WIDTH = HEIGHT = 600
@@ -7,6 +8,46 @@ TILE_SIZE = WIDTH / GAME_SIZE
 ASSETS = {}
 
 def loadAssets():
-    assetNames = 'whiteQueen', 'blackQueen', 'arrow', 'lightTile', 'darkTile', 'highlight'
+    assetNames = 'whiteQueen', 'blackQueen', 'arrow', 'highlight'
     for a in assetNames:
         ASSETS[a] = pg.image.load('../assets/' + a + '.png')
+
+def main():
+    pg.init()
+    screen = pg.displayset_mode((WIDTH, HEIGHT))
+    gameTime = pg.time.Clock
+    game = engine.GameState()
+    loadAssets()
+    running = True
+    
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+        drawBoard(screen, game)
+        gameTime.tick(10)
+        pg.display.flip()
+
+def drawBoard(screen, gamestate):
+    for x in range(GAME_SIZE):
+        for y in range(GAME_SIZE):
+            if ((x+y)%2==0):
+                colour = pg.color("white")
+            else:
+                colour = pg.color("beige")
+            pg.draw.rect(screen, colour, pg.Rect(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+
+            content = gamestate.game[x][y]
+            match content:
+                case Piece.arrow:
+                    pname = 'arrow'
+                case Piece.whiteQueen:
+                    pname = 'whiteQueen'
+                case Piece.blackQueen:
+                    pname = 'blackQueen'
+
+            screen.blit(ASSETS[pname], pg.Rect(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+
+
+if __name__ == '__main__':
+    main()
